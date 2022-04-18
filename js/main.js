bd22af0a0aedb25861dea067a2bdff0e1e34ac47
Akmaljon571 <ahmadjonovakmaljon571@yandex.ru>
2022-04-19 00:54:01 +0300

    js darslari

---
+++ js/main.js
@@ -0,0 +1,79 @@
+// 1-misol
+
+// let menu = {
+//     name:'salom'
+// }
+// function isEmpty() {
+//     if (Object.keys(menu).length===0) {
+//         return false;
+//     }
+//     return true;
+// }
+
+// console.log(isEmpty());
+
+// 2-misol
+
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pete: 130
+// }
+// let jami = 0;
+
+// function Pure() {
+//     for (let sum in salaries) {
+//       jami += salaries[sum]
+//       console.log(jami);
+//     }
+// }
+// Pure()
+
+// 3-misol
+
+// let menu = {
+//     width: 200,
+//     height: 300,
+//     title: "My menu"
+// };
+// let A = 0;
+
+// function jami() {
+//     for(let key in menu){
+//         if (!isNaN(menu[key])){
+//             A = Number(menu[key]*2)
+//         } 
+//         console.log(key + ' : ' + menu[key]);
+//     }
+// }
+
+// jami()
+
+// 4-misol
+
+// let arr = [5, 2, 1, -10, 8];
+
+// function sort(A,B) {
+//     return B-A
+// }
+
+// function sor(A,B) {
+//     return A - B
+// }
+
+// console.log(arr.sort(sort));
+// console.log(arr.sort(sor));
+
+// 5-misol
+
+// let sorov = Number(prompt('son kiriting'))
+// let jami = [];
+
+// function forLoop(A) {
+//     if (!isNaN(A)){
+//         let sorov = Number(prompt('son kiriting'))
+//         jami.push(sorov)
+//         console.log(jami);
+//         forLoop(sorov)
+//     }
+// }
